c595a0462c1f41ba3b203fe8904feaf10b229de4
makarova-anna <104019453+makarova-anna@users.noreply.github.com>
2022-05-22 13:32:00 +0300

    update 3
    reading data from files

--- main.cpp
+++ main.cpp
@@ -2,55 +2,76 @@
 #include <iostream>
 #include <algorithm>
 #include <list>
+#include <fstream>
+#include <filesystem>
 
 using namespace std;
 
-// функция для решения задачи
-int Solution(const int& N, const vector<int>& weights, const int& W, const vector<int>& values)
+
+int Solution(int N, vector<int> weights, int W, vector<int> values)
 {
-    int i, j;
-    vector< vector <int> > list(N+1, vector<int>(W + 1));
-    for (i = 0; i <= W; i++) 
+    vector<int> first(W+1, 0);
+    vector<int> second(W+1, 0);
+    int output = 0, i;
+    
+    for (i = 1; i <= N; i++) 
     {
-        list[0][i] = 0;
-    }
-    for (i = 1; i <= N; i++)
-    {
-        for (j = 0; j <= W; j++)
+        vector<int> tempo = first;
+        first = second;
+        second = tempo;
+        for (int j = 0; j <= W; j++)
         {
-            list[i][j] = list[i - 1][j];
-            if (j >= weights[i - 1])
+            second[j] = first[j];
+            if (j - weights[i - 1] >= 0) 
             {
-                list[i][j] = max(list[i][j], list[i-1][j - weights[i - 1]] + values[i - 1]);
+                second[j] = max(second[j], first[j - weights[i - 1]] + values[i - 1]);
             }
         }
     }
-    return list[N][W];
+    for (i = W; i >= 0; i--) 
+    {
+        if(second[i] != 0)
+        {
+            output = second[i];
+            break;
+        }
+    }
+    return output;
 }
 
-class thing
-{
-    public:
-        int v, w, n;
-        thing(int x, int y, int z) : v(x), w(y), n(z) {}
-};
+using namespace fsm = filesystem;
 
-int main()
+int main() 
 {
-    int N, W, i;
-    cin >> N >> W;
-    
-    vector<int> Weights;
-    vector<int> Values;
-   
-    for (i = 0; i < N; i++)
+    string path = "/Users/Анна/Desktop/backpack";
+    auto iterator = fsm::directory_iterator(path);
+    vector<fsm::path> Array;
+    copy_if(fsm::begin(iterator), fsm::end(iterator), std::back_inserter(Array), [](const auto& enter)
     {
-        int value, weight;
-        cin >> value >> weight;
-        Values.push_back(value);
-        Weights.push_back(weight);
+        return fsm::is_regular_file(enter);
+    });
+    for (auto& s : Array)
+    {
+        ifstream fin;
+        fin.open(s.string());
+        cout << s.string() << endl;
+        
+        int N, W;
+        fin >> N >> W;
+        
+        vector<int> Weights;
+        vector<int> Values;
+        
+        for (int i = 0; i < N; i++)
+        {
+            int value, weight;
+            fin >> value >> weight;
+            Values.push_back(value);
+            Weights.push_back(weight);
+        }
+        
+        int Maxx = Solution(N, Weights, W, Values);
+        cout << Maxx << endl;
     }
-    int Maxx = Solution(N, Weights, W, Values);
-    cout << Maxx << endl;
     return 0;
 }
