dcc568eaa2d191d1c25219e20885f2635b86bcb3
makarova-anna <104019453+makarova-anna@users.noreply.github.com>
2022-05-22 12:14:30 +0300

    update 1

--- main.cpp
+++ main.cpp
@@ -18,22 +18,39 @@ int Solution(const int& N, const vector<int>& weights, const int& W, const vecto
     {
         for (j = 0; j <= W; j++)
         {
-            list[i][j] = list[i-1][j];
+            list[i][j] = list[i - 1][j];
             if (j >= weights[i - 1])
             {
-                list[i][j] = max(list[i][j], list[i-1][j - weights[i-1]]);
+                list[i][j] = max(list[i][j], list[i-1][j - weights[i - 1]] + values[i - 1]);
             }
         }
     }
     return list[N][W];
 }
 
+class thing
+{
+    public:
+        int v, w, n;
+        thing(int x, int y, int z) : v(x), w(y), n(z) {}
+};
+
 int main()
 {
-    int N, W;
+    int N, W, i;
     cin >> N >> W;
-    vector<int> Values;
+    
     vector<int> Weights;
-
+    vector<int> Values;
+   
+    for (i = 0; i < N; i++)
+    {
+        int value, weight;
+        cin >> value >> weight;
+        Values.push_back(value);
+        Weights.push_back(weight);
+    }
+    int Maxx = Solution(N, Weights, W, Values);
+    cout << Maxx << endl;
     return 0;
 }
